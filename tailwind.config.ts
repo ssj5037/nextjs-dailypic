347800b4240913016fb6c0bd4fa88d98d8725c10
sujin shin <shinsj5037@gmail.com>
2024-02-16 20:41:45 +0900

    스크린 lg 사이즈 커스텀

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        lg: "1080px",
+      },
     },
   },
   plugins: [],
