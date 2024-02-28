2e81ceb3429ddec2541aa08194f4b8d67086b2de
신수진 <shinsj5037@gmail.com>
2024-02-28 21:39:32 +0900

    사용자 검색 debounce 적용

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,21 +1,24 @@
 'use client';
 
 import UserList from '@/component/search/UserList';
+import useDebounce from '@/hooks/useDebounce';
 import { useState } from 'react';
 
 export default function SearchPage() {
   const [search, setSearch] = useState('');
+  const debouncedSearch = useDebounce(search, 1000);
 
   return (
-    <div>
+    <div className='flex flex-col w-1/2 max-w-xl gap-10 min-w-96'>
       <input
         type='text'
+        autoFocus
         placeholder='사용자의 아이디 혹은 이름으로 검색해보세요.'
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        className='p-2 mb-10 border outline-none w-96'
+        className='p-2 border outline-none'
       />
-      <UserList search={search} />
+      <UserList search={debouncedSearch} />
     </div>
   );
 }
