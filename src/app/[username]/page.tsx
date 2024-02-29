3bc155e03431b5c49b02312a20b2e7d193467741
신수진 <shinsj5037@gmail.com>
2024-02-29 21:25:52 +0900

    동적 라우팅에 메타데이터 추가. 반복해서 요청되는 api를 동일한 파라미터에서는 캐시되도록 추가.(next에서 제공하는 cache api 이용)

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -3,16 +3,19 @@ import UserpagePostGrid from '@/component/userpage/UserpagePostGrid';
 import { ProfileUser } from '@/models/user';
 import { getUserProfile } from '@/service/user';
 import { notFound, redirect } from 'next/navigation';
+import { cache } from 'react';
 
 type Props = {
   params: { username: string };
 };
 
+const getUser = cache(async (username: string) => getUserProfile(username));
+
 export default async function UserpageLayout({ params: { username } }: Props) {
   if (!username) {
     redirect('/');
   }
-  const user: ProfileUser = await getUserProfile(username);
+  const user: ProfileUser = await getUser(username);
 
   if (!user) {
     notFound();
@@ -23,4 +26,12 @@ export default async function UserpageLayout({ params: { username } }: Props) {
       <UserpagePostGrid username={username} />
     </div>
   );
+}
+
+export async function generateMetadata({ params: { username } }: Props) {
+  const user = await getUser(username);
+  return {
+    title: `${user?.name} (@${user.username}) - DailyPic`,
+    description: `${user.name}의 DailyPic 입니다.`,
+  };
 }
