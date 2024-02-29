c958ce5c457045339a1e68be456b74876bcfa3d5
신수진 <shinsj5037@gmail.com>
2024-02-29 21:24:56 +0900

    메타데이터 추가

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,6 +3,12 @@ import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import ProviderLoginButton from './component/ProviderLoginButton';
 import { redirect } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '로그인',
+  description: 'DailyPic에 로그인/회원가입 하세요.',
+};
 
 export default async function SignInPage({
   searchParams: { callbackUrl },
@@ -17,10 +23,10 @@ export default async function SignInPage({
 
   const providers = await getProviders();
   return (
-    <div className='flex h-full justify-center'>
+    <div className='flex justify-center h-full'>
       <div className='flex items-center'>
-        <div className='border p-5 flex flex-col items-center gap-3 rounded border-orange-500'>
-          <h2 className='text-xl font-semibold p-5'>Sign In to DailyPic</h2>
+        <div className='flex flex-col items-center gap-3 p-5 border border-orange-500 rounded'>
+          <h2 className='p-5 text-xl font-semibold'>Sign In to DailyPic</h2>
           {providers &&
             Object.values(providers).map((provider) => (
               <ProviderLoginButton
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,7 +2,13 @@
 
 import UserList from '@/component/search/UserList';
 import useDebounce from '@/hooks/useDebounce';
+import { Metadata } from 'next';
 import { useState } from 'react';
+
+export const metadata: Metadata = {
+  title: '사용자 검색',
+  description: '팔로잉할 사용자를 검색하세요.',
+};
 
 export default function SearchPage() {
   const [search, setSearch] = useState('');
